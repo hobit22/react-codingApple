df9caf45e41e5cc3387625ddc97cb27168dcaa24
hobit22 <hobit22@naver.com>
2022-04-26 11:18:28 +0900

    useState

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
-
-  let posts = "zzz"
-  function 함수(){
-    return 100
-  }
-
+  let [title, titleFunction] = useState(["남자 코트 추천", "강남 우동 맛집"]);
+  let posts = "강남 고기 맛집"
+  
   return (
     <div className="App">
       <div className='black-nav'> 
         <div>개발 Blog</div>
       </div>
-      <h4>{ 함수() }</h4>
+      <div className='list'>
+        <h2>{ title[0] }</h2>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h2>{ title[1] }</h2>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
+      <div className='list'>
+        <h2>{ posts }</h2>
+        <p>2월 17일 발행</p>
+        <hr/>
+      </div>
     </div>
   );
 }
