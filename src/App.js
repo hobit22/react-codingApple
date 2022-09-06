f95aef381282ca88143e445b256d1bce47ad8d36
Hobin Kim <40729223+hobit22@users.noreply.github.com>
2022-09-06 23:46:06 +0900

    react 기초강의 6강

--- src/App.js
+++ src/App.js
@@ -33,8 +33,19 @@ function App() {
         <p>2월 17일 발행</p>
         <hr/>
       </div>
+      <Modal></Modal>
     </div>
   );
+}
+
+function Modal(){
+  return (
+    <div className='modal'>
+      <h2>제목</h2>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
